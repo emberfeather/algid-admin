46d93c38cf5bdf3fcf15f48278b8ea70f9fe6010
Randy Merrill <Zoramite@gmail.com>
2010-01-19 15:31:54 +0000

    Algid-admin #7: Disable submit after form submit
    Disabled the submit on form submission.

--- admin/extend/admin/theme/admin/script/admin.js
+++ admin/extend/admin/theme/admin/script/admin.js
@@ -11,6 +11,11 @@
 		// Focus on the first input in the content.
 		$('.content input[type=text]').focus();
 		
+		// Disable submit buttons on submit
+		$('form').submit(function(){
+			$('input[type=submit]', $(this)).attr('disabled', 'disabled');
+		});
+		
 		// Use the timeago plugin
 		$('abbr.timeago').timeago();
 	});
